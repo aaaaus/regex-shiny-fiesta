3c54466fbf04877b7af679499e9fa626ca5e177e
aaaaus <42099359+aaaaus@users.noreply.github.com>
2019-09-17 12:42:18 -0400

    Update examples.php

--- examples.php
+++ examples.php
@@ -2,6 +2,6 @@
 
 $date = '201909'; //4 digit year, 2 digit month
 //want to change format to first day of the month as YYYY-MM-DD
-$first_day = preg_replace('/([0-9]{4})([0-9}{2})/', '$1-$2-01', $date);
+$first_day = preg_replace('/([0-9]{4})([0-9}{2})/', '$1-$2-01', $date); //result: 2019-09-01
 //regex: two capturing groups used; first matches any character in the range of 0-9 for up to 4 matches (quantifier)
 //second capturing set matches the same range, but for up to 2 matches
