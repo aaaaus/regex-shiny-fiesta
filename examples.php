c7a1a27889874888221e793554fed9440efb3fb2
aaaaus <42099359+aaaaus@users.noreply.github.com>
2019-09-17 14:10:39 -0400

    Update examples.php

--- examples.php
+++ examples.php
@@ -5,3 +5,9 @@ $date = '201909'; //4 digit year, 2 digit month
 $first_day = preg_replace('/([0-9]{4})([0-9}{2})/', '$1-$2-01', $date); //result: 2019-09-01
 //regex: two capturing groups used; first matches any character in the range of 0-9 for up to 4 matches (quantifier)
 //second capturing set matches the same range, but for up to 2 matches
+
+$amount = '1234'; //this is the desired display amount x 100
+preg_replace( '/([0-9]{2})$/', "\.$1", $amount ); //result: '12.34'
+//regex: 1 capturing group used, looks for the final two digits to match a numeric range.
+//php: replaces the last two digits with '.digits' to display as currency
+
