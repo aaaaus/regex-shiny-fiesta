c11ee9bd0b52392daf7bdf578f338425aeaa12f2
aaaaus <42099359+aaaaus@users.noreply.github.com>
2019-09-20 14:41:53 -0400

    Update examples.php

--- examples.php
+++ examples.php
@@ -7,7 +7,7 @@ $first_day = preg_replace('/([0-9]{4})([0-9}{2})/', '$1-$2-01', $date); //result
 //second capturing set matches the same range, but for up to 2 matches
 
 $amount = '1234'; //this is the desired display amount x 100
-preg_replace( '/([0-9]{2})$/', "\.$1", $amount ); //result: '12.34'
+preg_replace( '/([0-9]{2})$/', ".$1", $amount ); //result: '12.34'
 //regex: 1 capturing group used, looks for the final two digits to match a numeric range.
 //php: replaces the last two digits with '.digits' to display as currency
 
